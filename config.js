3e4daf67c0f429ec490261481994d07c21debc37
Poddamods <140929117+PoddaMods@users.noreply.github.com>
2024-12-15 17:02:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", /* you sessin_id */
+SESSION_ID: process.env.SESSION_ID || "eVN0nbgL#62WKHYGi_kiXkHM3w19GUDvpgxoUaF0jVZ0zRAqH5T8", /* you sessin_id */
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
